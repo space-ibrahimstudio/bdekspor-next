79267849c082b55121e81e1b89c157ff57d8f47c
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-03-21 22:56:55 +0700

    ops: Add Gtag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { initGA, logPageView } from "@/components/utils/analytics";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
